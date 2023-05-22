cc207ed94e6ebb09f4e91f1fb2ec730da0522eef
Mursalin Mir <133050844+mursalinmirme@users.noreply.github.com>
2023-05-23 00:01:43 +0600

    add fronted file
    add front end file

---
+++ frontend/index.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include "../functions/function.php";
+
+if(!isset($_SESSION['user_unicid'])){
+  redirect("../signin.php","After signin you can visit user page!");
+}elseif(isset($_SESSION['user_unicid'])){
+if($_SESSION['user_roll_as'] == 'admin'){
+    redirect("../backend/admin.php","you are not autorized for user!");
+  }
+
+}
+
+
+if(isset($_SESSION['message'])){
+  echo $_SESSION['message'];
+}
+
+echo "<br>";
+
+echo "Welcome to user page here is situed for showing products";
+
+
+?>
